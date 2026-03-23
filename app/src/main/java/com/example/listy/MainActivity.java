70a6dea9185c1e6e950a4de67f795f1323a2bebf
= <19tomek11@gmail.com>
2026-03-23 14:17:32 +0100

    na 3

--- app/src/main/java/com/example/listy/MainActivity.java
+++ app/src/main/java/com/example/listy/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.listy;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String>jezykiArrayList;
+    private ArrayAdapter<String>arrayAdapter;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        jezykiArrayList = new ArrayList<>();
+        jezykiArrayList.add("Pearl");
+        jezykiArrayList.add("Ruby");
+        jezykiArrayList.add("Flutter");
+        jezykiArrayList.add("Kotlin");
+        jezykiArrayList.add("Go");
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, jezykiArrayList);
+
+        listView = findViewById(R.id.listViewJezyki);
+        listView.setAdapter(arrayAdapter);
     }
 }
